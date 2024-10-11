82decea47f0f52be159b4b9ab0b6ad0a06394b94
lexdeit <godhood@proton.me>
2024-10-10 21:39:57 -0600

    added images to rss

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,6 +1,10 @@
 import type { APIRoute } from 'astro';
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
+import sanitizeHtml from "sanitize-html";
+import MarkdownIt from 'markdown-it';
+
+const parser = new MarkdownIt();
 
 export const GET: APIRoute = async ({ params, request, site }) => {
 
@@ -11,11 +15,22 @@ export const GET: APIRoute = async ({ params, request, site }) => {
         title: 'Emmanuel`s Blog',
         description: 'A humble Astronaut’s guide to the stars',
         site: site ?? '',
-        items: posts.map(({ data, slug }) => ({
+        items: posts.map(({ data, slug, body }) => ({
             title: data.title,
             pubDate: data.date,
             description: data.description,
-            link: `posts/${slug}`
+            link: `posts/${slug}`,
+            content: sanitizeHtml(parser.render(body), {
+                allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
+              }),
+              
+              customData: `<media:content
+                  type="image/${data.image.format === 'jpg' ? 'jpeg' : 'png'}"
+                  width="${data.image.width}"
+                  height="${data.image.height}"
+                  medium="image"
+                  url="${site + data.image.src}" />
+              `,
         })),
         customData: `<language>es-mx</language>`,
     });
