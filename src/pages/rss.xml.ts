be449bea012563de84e16d8892ecd6d59e72c514
lexdeit <godhood@proton.me>
2024-10-10 21:12:33 -0600

    chore: added rss config

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,9 +1,21 @@
 import type { APIRoute } from 'astro';
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 
-export const GET: APIRoute = ({ params, request }) => {
-    return new Response(JSON.stringify({
-        hola: "Mundo",
-        name: "Emmanuel"
-    })
-    );
+export const GET: APIRoute = async ({ params, request, site }) => {
+
+    const posts = await getCollection('blog');
+
+    return rss({
+        title: 'Emmanuel`s Blog',
+        description: 'A humble Astronaut’s guide to the stars',
+        site: site ?? '',
+        items: posts.map(({ data, slug }) => ({
+            title: data.title,
+            pubDate: data.date,
+            description: data.description,
+            link: `posts/${slug}`
+        })),
+        customData: `<language>es-mx</language>`,
+    });
 }
