a484f5c0f7497bf0c8c2a92f7f38b64eb1900683
lexdeit <godhood@proton.me>
2024-10-17 23:20:16 -0600

    docs: added comments at xml generator

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -3,7 +3,7 @@ import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import sanitizeHtml from "sanitize-html";
 import MarkdownIt from 'markdown-it';
-
+//Este archivo retorna un xml para el newslester
 const parser = new MarkdownIt();
 
 export const GET: APIRoute = async ({ params, request, site }) => {
